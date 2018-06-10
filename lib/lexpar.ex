2261cf0c80ebef4057630268f58a1509cff74b61
Delyan Dobrev <delqn.dobrev@gmail.com>
2018-06-10 11:37:59 +0300

    Initial commit

---
+++ lib/lexpar.ex
@@ -0,0 +1,18 @@
+defmodule Lexpar do
+  @moduledoc """
+  Documentation for Lexpar.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Lexpar.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/lexpar_test.exs
@@ -0,0 +1,8 @@
+defmodule LexparTest do
+  use ExUnit.Case
+  doctest Lexpar
+
+  test "greets the world" do
+    assert Lexpar.hello() == :world
+  end
+end
